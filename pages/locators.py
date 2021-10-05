06868d4f1ec7d42d45266184afe2540e910cf19f
domrachevaa <domracheva.a@rambler.ru>
2021-10-05 19:14:42 +0700

    Update locator.py
    Was added class LoginPageLocators.

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,15 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    reg_form = (By.CSS_SELECTOR, "form#register_form")
+    '''reg_email = (By.CSS_SELECTOR, 'input[name="registration-email"')
+    reg_password = (By.CSS_SELECTOR, 'input[name="registration-password1"')'''
+
+    login_form = (By.CSS_SELECTOR, "form#login_form")
+    '''login_email = (By.CSS_SELECTOR, 'input[name="login-username"]')
+    login_password = (By.CSS_SELECTOR, 'input[name="login-password"]')'''
+
+
